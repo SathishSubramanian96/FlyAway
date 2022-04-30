c587b1bfb6c37a2e30e1e24efe745b719b1e05a6
Sathish Subramanian <99395886+SathishSubramanian96@users.noreply.github.com>
2022-04-30 09:04:09 +0530

    Deleted password

--- src/main/java/com/database/Dao.java
+++ src/main/java/com/database/Dao.java
@@ -20,7 +20,7 @@ public class Dao {
 
 	public Dao() throws ClassNotFoundException, SQLException{
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/flyaway","root","Subbu_1967");
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/flyaway","root","");
 		System.out.println("connection established with database");
 		st=con.createStatement();
 		
